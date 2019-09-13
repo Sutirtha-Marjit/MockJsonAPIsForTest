31313f9b93ad3539c5309ab1f2e313a364d316f7
Sutirtha Marjit <sutirtha.social.marjit@gmail.com>
2019-09-13 08:46:01 +0530

    heroku fix

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = new express();
+app.set('port',process.env.port || 5000);
 
 app.get('/',(request,response)=>{
     response.json({
         text:'welcome to api store'
     });
+}).listen(app.get('port'),()=>{
+  console.log(`App is running at ${app.get('port')}`);
 });
 
-app.listen(process.env.port,()=>{
-    console.log(`App started`);
-})
